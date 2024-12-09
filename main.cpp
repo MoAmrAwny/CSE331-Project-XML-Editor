2b6e7c263ea57bf4fe557e40babd3ee2d3f20240
maryamyasser33 <maryamyasser303@gmail.com>
2024-12-09 19:26:07 +0200

    .

--- main.cpp
+++ main.cpp
@@ -24,15 +24,16 @@ public:
 int main() {
 
     ifstream file( "sample.xml");
+    ofstream outputFile( "sample.json");
 
     string xmlstring;
     vector<user>users;
 while(getline(file,xmlstring)) {
-    cout << xmlstring << endl;
+    //cout << xmlstring << endl;
 
     if (xmlstring == "<users>") {
         while (getline(file, xmlstring)) {
-
+            //cout << xmlstring << endl;
             size_t pos1 = xmlstring.find('<');
             size_t pos2 = xmlstring.find('>');
             if (xmlstring.substr(pos1 + 1, pos2 - pos1 - 1) == "user") {
@@ -56,7 +57,7 @@ while(getline(file,xmlstring)) {
                     }
                     else if (checkObject == "posts") {
                         while (getline(file, xmlstring)) {
-                            cout << xmlstring << endl;
+                            //cout << xmlstring << endl;
 
                             pos1 = xmlstring.find('<');
                             pos2 = xmlstring.find('>');
@@ -65,7 +66,7 @@ while(getline(file,xmlstring)) {
                             else if (checkObject == "post") {
                                 post post;
                                 while (getline(file, xmlstring)) {
-                                    cout << xmlstring << endl;
+                                    //cout << xmlstring << endl;
 
                                     pos1 = xmlstring.find('<');
                                     pos2 = xmlstring.find('>');
@@ -73,7 +74,7 @@ while(getline(file,xmlstring)) {
                                     if (chechObjTopic == "/post")break;
                                     else if (chechObjTopic == "body") {
                                         while (getline(file, xmlstring)) {
-                                            cout << xmlstring << endl;
+                                            //cout << xmlstring << endl;
 
                                             pos1 = xmlstring.find('<');
                                             pos2 = xmlstring.find('>');
@@ -85,7 +86,7 @@ while(getline(file,xmlstring)) {
                                         }
                                     } else if (chechObjTopic == "topics") {
                                         while (getline(file, xmlstring)) {
-                                            cout << xmlstring << endl;
+                                            //cout << xmlstring << endl;
 
                                             pos1 = xmlstring.find('<');
                                             pos2 = xmlstring.find('>');
@@ -93,7 +94,7 @@ while(getline(file,xmlstring)) {
                                             if (checkObject == "/topics")break;
                                             else if (checkObject == "topic") {
                                                 while (getline(file, xmlstring)) {
-                                                    cout << xmlstring << endl;
+                                                    //cout << xmlstring << endl;
 
                                                     pos1 = xmlstring.find('<');
                                                     pos2 = xmlstring.find('>');
@@ -111,7 +112,7 @@ while(getline(file,xmlstring)) {
                         }
                     } else if (checkObject == "followers") {
                         while (getline(file, xmlstring)) {
-                            cout << xmlstring << endl;
+                            //cout << xmlstring << endl;
 
                             pos1 = xmlstring.find('<');
                             pos2 = xmlstring.find('>');
@@ -119,7 +120,7 @@ while(getline(file,xmlstring)) {
                             if (checkObject == "/followers")break;
                             else if (checkObject == "follower") {
                                 while (getline(file, xmlstring)) {
-                                    cout << xmlstring << endl;
+                                    //cout << xmlstring << endl;
 
                                     pos1 = xmlstring.find('<');
                                     pos2 = xmlstring.find('>');
@@ -138,84 +139,203 @@ while(getline(file,xmlstring)) {
         }
     }
 }
-cout<<"{"<<endl;
-cout<<R"("users": [)"<<endl;
 
+int indentation=0;
+
+
+outputFile<<"{"<<endl;
+indentation++;
+
+int temp=indentation;
+    while (temp--)outputFile<<"    ";
+    indentation++;
+outputFile<<R"("users": [)"<<endl;
+
+     temp=indentation;
+    while (temp--)outputFile<<"    ";
     for (int i=0;i<users.size();i++) {
-        cout<<"{"<<endl;
-        cout<<R"("id": ")"<<users[i].id<<"\","<<endl;
-        cout<<R"("name": ")"<<users[i].name<<"\","<<endl;
-        cout<<R"("posts": )"<<"["<<endl;
-        cout<<"{"<<endl;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"{"<<endl;
+        indentation++;
 
+         temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<R"("id": ")"<<users[i].id<<"\","<<endl;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<R"("name": ")"<<users[i].name<<"\","<<endl;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<R"("posts": )"<<"["<<endl;
+        indentation++;
 
-        cout<<R"("post": )"<<"["<<endl;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"{"<<endl;
 
+        indentation++;
+
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<R"("post": )"<<"["<<endl;
+        indentation++;
             for (int j = 0; j < users[i].posts.size(); ++j) {
 
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+            outputFile<<"{"<<endl;
+                indentation++;
 
-            cout<<"{"<<endl;
-            cout<<R"("body": ")";
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+            outputFile<<R"("body": ")";
            string body= users[i].posts[j].body;
                 std::reverse(body.begin(), body.end());
                 while(body.back()==' ')body.pop_back();
                 std::reverse(body.begin(), body.end());
 
-            cout<<body<<"\","<<endl;
-            cout<<R"("topics": )"<<"["<<endl;
+            outputFile<<body<<"\","<<endl;
 
-            cout<<"{"<<endl;
-                cout<<R"("topic": )";
-                if(users[i].posts[j].topics.size()>1)cout<<"["<<endl;
+
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+            outputFile<<R"("topics": )"<<"["<<endl;
+                indentation++;
+
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+            outputFile<<"{"<<endl;
+                indentation++;
+
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+                outputFile<<R"("topic": )";
+                if(users[i].posts[j].topics.size()==1) {  outputFile<<"\"";
+
+                    string topic= users[i].posts[j].topics[0];
+                    std::reverse(topic.begin(), topic.end());
+                    while(topic.back()==' ')topic.pop_back();
+                    std::reverse(topic.begin(), topic.end());
+
+
+                    outputFile<<topic<<"\""<<endl;}
+                else{
+                    outputFile << "[" << endl;
+                indentation++;
 
             for (int k=0;k<users[i].posts[j].topics.size();k++) {
-                cout<<"\"";
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+                outputFile<<"\"";
 
                 string topic= users[i].posts[j].topics[k];
                 std::reverse(topic.begin(), topic.end());
                 while(topic.back()==' ')topic.pop_back();
                 std::reverse(topic.begin(), topic.end());
 
-                cout<<topic<<"\"";
-                if(k<users[i].posts[j].topics.size()-1)cout<<",";
-                cout<<endl;
+
+                outputFile<<topic<<"\"";
+                if(k<users[i].posts[j].topics.size()-1)outputFile<<",";
+                outputFile<<endl;
             }
-                if(users[i].posts[j].topics.size()>1)cout<<"]"<<endl;
+                }
 
-            cout<<"}"<<endl;
-            cout<<"]"<<endl;
-            cout<<"}";
-            if(j<users[i].posts.size()-1)cout<<",";
-            cout<<endl;
-        }            cout<<"]"<<endl;
+                if(users[i].posts[j].topics.size()>1) {  indentation--;
+                    temp=indentation;
+                    while (temp--)outputFile<<"    ";
+                    outputFile << "]" << endl; }
+                indentation--;
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+            outputFile<<"}"<<endl;
+                indentation--;
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+            outputFile<<"]"<<endl;
+                indentation--;
+                temp=indentation;
+                while (temp--)outputFile<<"    ";
+            outputFile<<"}";
+            if(j<users[i].posts.size()-1)outputFile<<",";
+            outputFile<<endl;
+        }         indentation--;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";   outputFile<<"]"<<endl;
 
 
+        indentation--;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+
+        outputFile<<"}"<<endl;
+        indentation--;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"],"<<endl;
 
 
-        cout<<"}"<<endl;
-        cout<<"],"<<endl;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+
+        outputFile<<R"("followers": )"<<"["<<endl;
+        indentation++;
+
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"{"<<endl;
+        indentation++;
+
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<R"("follower": )"<<"["<<endl;
+        indentation++;
 
 
-
-
-        cout<<R"("followers": )"<<"["<<endl;
-        cout<<"{"<<endl;
-        cout<<R"("follower": )"<<"["<<endl;
         for (int l=0;l<users[i].followers.size();l++) {
-            cout<<"{"<<endl;
-            cout<<R"("id": ")"<<users[i].followers[l]<<"\""<<endl;
-            cout<<"}";
-            if(l<users[i].followers.size()-1)cout<<",";
-            cout<<endl;
-        }cout<<"]"<<endl;
-        cout<<"}"<<endl;
-        cout<<"]"<<endl;
-        cout<<"}";
-        if(i<users.size()-1)cout<<",";
-        cout<<endl;
+            temp=indentation;
+            while (temp--)outputFile<<"    ";
+            outputFile<<"{"<<endl;
+            indentation++;
+            temp=indentation;
+            while (temp--)outputFile<<"    ";
+            outputFile<<R"("id": ")"<<users[i].followers[l]<<"\""<<endl;
+            indentation--;
+            temp=indentation;
+            while (temp--)outputFile<<"    ";
+            outputFile<<"}";
+            if(l<users[i].followers.size()-1)outputFile<<",";
+
+            outputFile<<endl;
+        }
+
+        indentation--;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"]"<<endl;
+        indentation--;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"}"<<endl;
+        indentation--;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"]"<<endl;
+        indentation--;
+        temp=indentation;
+        while (temp--)outputFile<<"    ";
+        outputFile<<"}";
+        if(i<users.size()-1)outputFile<<",";
+        outputFile<<endl;
     }
-    cout<<"]"<<endl;
-    cout<<"}";
-    ofstream outputFile( "sample.json");
+    indentation--;
+    temp=indentation;
+    while (temp--)outputFile<<"    ";
+    outputFile<<"]"<<endl;
+    indentation--;
+    temp=indentation;
+    while (temp--)outputFile<<"    ";
+    outputFile<<"}";
+    //cout<<"indent"<<indentation;
 
 }
