c64e8187e20c59f4ab4a6a8dda4ab48b05a91ddc
Mohamed Amr <159016297+MoAmrAwny@users.noreply.github.com>
2024-12-28 20:24:03 +0200

    Update and rename Error_detection_correction.h to ErrorHandling.h

---
+++ ErrorHandling.h
@@ -0,0 +1,17 @@
+#ifndef ERROR_DETECTION_CORRECTION_H
+#define ERROR_DETECTION_CORRECTION_H
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+void errorHandling
+
+
+
+#endif //ERROR_DETECTION_CORRECTION_H
