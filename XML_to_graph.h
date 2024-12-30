d7285d5e910fd437c21ee1690dba346e29c2d048
AbdelrahmanSomethingSomething <167958380+AbdelrahmanSomethingSomething@users.noreply.github.com>
2024-12-30 22:54:20 +0200

    Add files via upload

---
+++ XML_to_graph.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+#include<fstream>
+using namespace std;
+
+class post {
+public:
+    string body;
+    vector<string>topics;
+};
+class user {
+public:
+    int id;
+    string name;
+    vector<post>posts;
+    vector<int>followers;
+    user(); 
+};
+
+
+void xmlToGraph(const string& inputname, const string& outputname);
+
+void visualizeGraph(const string& dotFile, const string& outputFile);
