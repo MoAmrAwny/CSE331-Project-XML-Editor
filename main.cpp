3804df1c5626b5617a1d3d8094aeac7dbe0a4ffa
Mohamed Amr <159016297+MoAmrAwny@users.noreply.github.com>
2024-12-31 01:37:47 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Minifying.h"
-
-int main() {
-    string fileName = "minified.xml"; // Specify the XML file to minify
-    string output= "sample (1).xml" ;
-    minifying(fileName,output);               // Call the function to process the file
-    return 0;
-}
