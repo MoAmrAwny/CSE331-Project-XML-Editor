5dc06d57f69f52de9a237e7a4dd7167afffad6d8
Ganna Allah Ayman Abdullah Mostafa Gawish <153089309+gannagawish@users.noreply.github.com>
2024-12-30 22:48:02 +0200

    Update network_analysis.h

--- network_analysis.h
+++ network_analysis.h
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
 
+class post {
+public:
+    string body;
+    vector<string>topics;
+};
+class user {
+public:
+    int id;
+    string name;
+    vector<post>posts;
+    vector<int>followers;
+    user();
+};
+
+user::user() {
+    id = 0;
+    name = "";
+}
+
+vector<user> xmlToGraph(const string& inputname);
+vector<pair<string,int>> MostInfluencer(vector<user> users);
+
+vector<pair<string,int>> MutualUsers(vector<user>users,vector<string>v);
+
+vector<pair<string,int>> MostActive(vector<user>users);
+
+vector<pair<string,int>> SuggestFollowers(vector<user>users,string idd);
