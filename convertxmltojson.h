65524debdf26fd43fe3b9e2f83e81ce94646b625
maryamyasser33 <168692130+maryamyasser33@users.noreply.github.com>
2025-01-01 16:46:10 +0200

    Update convertxmltojson.h

--- convertxmltojson.h
+++ convertxmltojson.h
@@ -1,16 +1,11 @@
-
 #pragma once
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <stack>
-using namespace std;
+#include "../generalHeader.h"
 
-void indentate(int indentation, ofstream &outputFile);
+void indent(int indentation, ofstream &outputFile);
 void closeTag(string &jsonString,stack<char>&st);
-void arrayTag(string tagName,string &jsonString,stack<char>&st);
-void sentencearrayTag(string tagName, string sentence,string &jsonString,stack<char>&st) ;
-void sentenceTag(string tagName, string sentence,string &jsonString);
-void normalTag(string tagName,string &jsonString,stack<char>&st);
-void justsentence(string sentence,string &jsonString);
+void arrayTag(const string& tagName,string &jsonString,stack<char>&st);
+void sentenceArrayTag(const string& tagName, const string& sentence, string &jsonString, stack<char>&st) ;
+void sentenceTag(const string& tagName, const string& sentence,string &jsonString);
+void normalTag(const string& tagName,string &jsonString,stack<char>&st);
+void justSentence(const string& sentence, string &jsonString);
 void convertXMLtoJSON(ifstream &file, ofstream &outputFile );
