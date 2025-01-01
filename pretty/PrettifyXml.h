e2461fded4c64ab124854258380d18c98979ad06
maryamyasser33 <168692130+maryamyasser33@users.noreply.github.com>
2025-01-01 17:30:00 +0200

    Add files via upload

---
+++ pretty/PrettifyXml.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "../generalHeader.h"
+
+class XmlNode {
+public:
+    string name;                   // Name of the node
+    string value;                  // Value of the node (text content)
+    vector<XmlNode*> children;      // Child nodes multiple children is possible
+};
+
+
+
+//helper functions
+void debug_tree(XmlNode* node, int level);
+
+string trim(const std::string& str);
+//parsing xml into tree structure
+XmlNode* parse_node(const vector<string>& s, int& idx);
+
+void print_node(XmlNode* node, ostringstream& out, int level);
+
+vector<string> ReadXml(ifstream& filename);
+
+void PrettifyXML(vector<string> input_xml,ofstream &outputFile);
